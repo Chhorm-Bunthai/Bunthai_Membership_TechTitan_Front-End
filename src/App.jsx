c1145d7a41a6a866e861712f9da29d5465467614
Chhorm Bunthai <bunthaicambo89@gmail.com>
2023-12-20 20:02:59 +0700

    add signup and login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import './App.css'
+import "./App.css";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-
   return (
-    <>
-     My App!
-    </>
-  )
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/SignUp" element={<SignUp />}/>
+    //   </Routes>
+    //   <Routes>
+    //   <Route path="/SignIn" element={<SignIn />}/>
+    //   </Routes>
+    // </BrowserRouter>
+    <div><SignIn /></div>
+  );
 }
 
-export default App
+export default App;
