ef83b3955c24b873ab558082e0e5b16f47566a4e
Chhorm Bunthai <bunthaicambo89@gmail.com>
2023-12-26 10:40:24 +0700

    I already fix bug by adding home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import "./App.css";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/Login";
 import ForgotPassword from "./pages/ForgotPassword";
+import HomePage from "./pages/HomePage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route index path="/" element={<SignIn />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<SignIn />} />
-        <Route path="/forgotpassword" element={<ForgotPassword />}/>
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
       </Routes>
     </BrowserRouter>
   );
